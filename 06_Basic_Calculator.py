567c5aa03a2486c251a44381c00880334ab4ff6c
dochman-ato <daniel.ochman@atos.net>
2019-06-24 15:49:49 +0200

    Commit with User input and Basic calculator tasks.

---
+++ 06_Basic_Calculator.py
@@ -0,0 +1,11 @@
+#1 input zwraca stringi wiec trzeba rzutowac
+num1 = float(input("Please provide first number: "))
+num2 = float(input("Please provide second number: "))
+result = num1 + num2
+print(result)
+#2
+result2 = num1 + num2
+print(result2)
+#3
+result3 = float(num1) + float(num2)
+print(result3)
