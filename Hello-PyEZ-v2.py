43373e5bfc483adf04d2764b68ad81867c5119c6
dochman-ato <daniel.ochman@atos.net>
2019-06-26 15:05:28 +0200

    Commit with PyEZ version 2 scrypt (input username and hostname and password)

---
+++ Hello-PyEZ-v2.py
@@ -0,0 +1,19 @@
+from pprint import pprint
+from jnpr.junos import Device
+import getpass
+
+host = None
+uname = None
+pw = None
+
+if host == None:
+    host = input("Hostname or IP: ")
+if uname == None:
+    uname = input("Username: ")
+if pw == None:
+    pw = getpass.getpass()
+
+dev = Device(host=host, user=uname, password=pw)
+dev.open()
+pprint(dev.facts)
+dev.close()
