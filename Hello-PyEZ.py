4885e88fe2b676f6ecb756b57cd874ee9a2b9366
dochman-ato <daniel.ochman@atos.net>
2019-06-26 14:11:39 +0200

    Commit with PyEZ scrypt

--- Hello-PyEZ.py
+++ Hello-PyEZ.py
@@ -1,6 +1,8 @@
 from pprint import pprint
 from jnpr.junos import Device
 
+#funkcje nie sa identyfikowane przez to, ze one beda zaciagane dopiero na linuxie
+
 dev = Device(host = "172.25.11.1", user = "lab", password = "lab123")
 dev.open()
 pprint(dev.facts)
